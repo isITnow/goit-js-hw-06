6cd757fc86effcec42bf6cf58eb9465e3199bb48
Roman Serediuk <box4rom@gmail.com>
2022-07-31 15:37:13 +0300

    project setup

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+const numberOfcategories = document.querySelectorAll('.item').length;
+console.log(`Number of categories: ${numberOfcategories}`);
+
+const ulElem = document.querySelector('#categories');
+console.log(ulElem);
+
+const ulItem = ulElem.children;
+console.log(ulItem);
