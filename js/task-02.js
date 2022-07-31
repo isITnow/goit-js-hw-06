62013d2aa8fecc2edd32706e32463d6c82257edb
Roman Serediuk <box4rom@gmail.com>
2022-07-31 18:06:09 +0300

    task -02 completed

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,22 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+    'Potatoes',
+    'Mushrooms',
+    'Garlic',
+    'Tomatos',
+    'Herbs',
+    'Condiments',
 ];
+
+const createIngredietsListItems = ingredients => {
+    return ingredients.map(item => {
+        const ulItem = document.createElement('li');
+        ulItem.classList.add('item');
+        ulItem.textContent = [item];
+        return ulItem;
+    });
+};
+
+const ingredietsItems = createIngredietsListItems(ingredients);
+
+const ingredientsList = document.querySelector('#ingredients');
+ingredientsList.append(...ingredietsItems);
