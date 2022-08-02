64fb827b46bdc44478acdfa132b8319ca6de12c6
Roman Serediuk <box4rom@gmail.com>
2022-08-02 18:00:15 +0300

    task-09 completed

--- js/task-08.js
+++ js/task-08.js
@@ -6,12 +6,13 @@ function onFormSubmit(event) {
     event.preventDefault();
     let { email, password } = event.target.elements;
     if (email.value === '' || password.value === '') {
-        return alert('Поля мають бути заповнені! Хіба не зрозуміло?!');
-    }
-    email = email.value;
-    password = password.value;
-    const objData = { email, password };
+        alert('Поля мають бути заповнені! Хіба не зрозуміло?!');
+    } else {
+        email = email.value;
+        password = password.value;
+        const objData = { email, password };
 
-    console.log(objData);
-    event.target.reset();
+        console.log(objData);
+        event.target.reset();
+    }
 }
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const btnEl = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+
+btnEl.addEventListener('click', onBtnClick);
+
+function onBtnClick() {
+    const color = getRandomHexColor();
+    document.body.style.backgroundColor = `${color}`;
+    spanEl.textContent = `${color}`;
 }
