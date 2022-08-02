b0a703934c037d60f8c65bdb7b9bf07d770b475d
Roman Serediuk <box4rom@gmail.com>
2022-08-02 18:12:27 +0300

    final bugfix

--- js/task-02.js
+++ js/task-02.js
@@ -7,16 +7,17 @@ const ingredients = [
     'Condiments',
 ];
 
+const ingredientsList = document.querySelector('#ingredients');
+
 const createIngredietsListItems = ingredients => {
     return ingredients.map(item => {
         const ulItem = document.createElement('li');
         ulItem.classList.add('item');
-        ulItem.textContent = [item];
+        ulItem.textContent = item;
         return ulItem;
     });
 };
 
 const ingredietsItems = createIngredietsListItems(ingredients);
 
-const ingredientsList = document.querySelector('#ingredients');
 ingredientsList.append(...ingredietsItems);
--- js/task-04.js
+++ js/task-04.js
@@ -1,18 +1,16 @@
 let counterValue = 0;
 
 const btnDecrement = document.querySelector('[data-action="decrement"]');
-
 const btnIncrement = document.querySelector('[data-action="increment"]');
-
-let number = document.querySelector('#value');
+const spanEl = document.querySelector('#value');
 
 btnIncrement.addEventListener('click', onIncrementClick);
 btnDecrement.addEventListener('click', onDecrementClick);
 
 function onIncrementClick() {
-    return (number.textContent = counterValue += 1);
+    return (spanEl.textContent = counterValue += 1);
 }
 
 function onDecrementClick() {
-    return (number.textContent = counterValue -= 1);
+    return (spanEl.textContent = counterValue -= 1);
 }
